48897c47561d8f283a54e5e4206482c0b0d5b545
shawnlxh <657747939@qq.com>
2016-08-11 14:52:10 +0800

    Add files via upload

---
+++ Contrast_Experiment/SVR/svr_get_loss.py
@@ -0,0 +1,28 @@
+import os
+#os.chdir('desktop/libsvm-3.21/python')
+f1 = open('test_data')
+f2 = open('label')
+
+label1 = []
+for i in f1:
+    i = i.split(' ')
+    i = i[0]
+    i = float(i) * 127 + 29
+    label1.append(i)
+    
+
+
+label2 = []
+for i in f2:
+    i = float(i) * 127 + 29
+    label2.append(i)
+    
+loss = 0
+loss2 = 0
+count = 0
+for i in label1:
+    loss += abs(label1[count] - label2[count])
+    count += 1
+    
+loss = float(loss) / count
+print loss
